e626dee8fa38b8c67d404b683f49c4285c3163b7
Joe Klause <joeklause@gmail.com>
2019-10-29 13:40:04 -0700

    App updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Character from './Character';
+import List from './List';
+import charArray from '../data/charArray.js';
 
 
 const App = () => (
   <>
     <Header />
+    <List
+      items={charArray}
+      Component={Character} />
     <Footer />
   </>
 );
