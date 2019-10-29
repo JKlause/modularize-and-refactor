a546db3ddf0446d109aea5d45047714a430c5a70
Joe Klause <joeklause@gmail.com>
2019-10-29 13:39:57 -0700

    List component written

---
+++ src/components/List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const List = ({ items, Component }) => {
+
+  const myComponents = items.map(item => {
+    // eslint-disable-next-line react/jsx-key
+    return <Component {...item}/>;
+  });
+
+  return (
+    <>
+    Component{myComponents}
+    </>
+  );
+};
+
+export default List;
+
+List.propTypes = {
+  items: PropTypes.array.isRequired,
+  Component: PropTypes.func.isRequired,
+};
+
