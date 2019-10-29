286fda452d9df1647133336804348bff025cd2fb
Joe Klause <joeklause@gmail.com>
2019-10-29 14:12:31 -0700

    completed charArray

---
+++ src/data/charArray.js
@@ -0,0 +1,59 @@
+
+// eslint-disable-next-line no-undef
+export default [
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    name: 'Rick Sanchez',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+    name: 'Morty Smith',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+    name: 'Summer Smith',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+    name: 'Beth Smith',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+    name: 'Jerry Smith',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/11.jpeg',
+    name: 'Albert Einstein',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/14.jpeg',
+    name: 'Alien Morty',
+    species: 'Alien'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/15.jpeg',
+    name: 'Alien Rick',
+    species: 'Alien'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/17.jpeg',
+    name: 'Annie',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/18.jpeg',
+    name: 'Antenna Morty',
+    species: 'Human'
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/19.jpeg',
+    name: 'Antenna Rick',
+    species: 'Human'
+  },
+];
